e09e231c546bdca8e1b28afdfaa931dc20869845
Rich Megginson <rmeggins@redhat.com>
2022-09-07 12:53:00 -0600

    debug vault

--- src/tox_lsr/test_scripts/run_test_pb_localhost.sh
+++ src/tox_lsr/test_scripts/run_test_pb_localhost.sh
@@ -135,6 +135,7 @@ setup_vault() {
             unset ANSIBLE_VAULT_PASSWORD_FILE
             vault_args=""
         fi
+        ansible -c local localhost -m debug -a var=my_secret_var -e "@$vault_vars_file"
     else
         unset ANSIBLE_VAULT_PASSWORD_FILE
         vault_args=""
